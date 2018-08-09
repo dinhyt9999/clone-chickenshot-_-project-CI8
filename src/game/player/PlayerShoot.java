7c6227569f5606e191cef9d51ecb8a7b0b8a2c03
DinhOcCho <maybaygiay1998@gmail.com>
2018-08-09 11:23:06 +0700

    thay đổi tốc độ đạn

--- src/game/player/PlayerShoot.java
+++ src/game/player/PlayerShoot.java
@@ -1,18 +1,21 @@
 package game.player;
 
 import base.Attribute;
+import base.FrameCounter;
 import base.GameObjectManager;
 import input.KeyboardEvent;
 
 public class PlayerShoot implements Attribute<Player> {
+    private FrameCounter frameCounter = new FrameCounter();
 
     @Override
     public void run(Player gameObject) {
-        if (KeyboardEvent.instance.isSpace) {
+        if (KeyboardEvent.instance.isSpace && frameCounter.compare(10)) {
             BulletPlayer bulletPlayer = new BulletPlayer();
-            bulletPlayer.position.set(gameObject.position);
+            bulletPlayer.position.set(gameObject.position.x - 3,gameObject.position.y - 19);
             bulletPlayer.velocity.set(0,-4.5f);
             GameObjectManager.instance.add(bulletPlayer);
         }
+        frameCounter.run();
     }
 }
