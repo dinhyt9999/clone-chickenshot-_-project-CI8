44be92254bc2b0b3f6ef10b7cb9ce019ecc0cbae
Bài 1+2 <huytit98@gmail.com>
2018-08-21 22:03:14 +0700

    Update Animation

--- src/renderer/ImageRenderer.java
+++ src/renderer/ImageRenderer.java
@@ -6,6 +6,7 @@ import java.awt.*;
 import java.awt.image.BufferedImage;
 import java.io.File;
 import java.io.IOException;
+import utils.Utils;
 
 public class ImageRenderer implements Renderer {
     private BufferedImage image;
@@ -13,19 +14,21 @@ public class ImageRenderer implements Renderer {
     private int height;
 
     public ImageRenderer(String path,int width,int height){
-        this.image = this.loadImage(path);
+        this.image = Utils.loadImage(path);
         this.width = width;
         this.height = height;
     }
-    private BufferedImage loadImage(String path) {
-        try {
-            return ImageIO.read(new File(path));
-        } catch (IOException e) {
-            return null;
-        }
-    }
+//    private BufferedImage loadImage(String path) {
+//        try {
+//            return ImageIO.read(new File(path));
+//        } catch (IOException e) {
+//            return null;
+//        }
+//    }
     @Override
     public void render(Graphics graphics, Vector2D position) {
-            graphics.drawImage(this.image,(int)position.x,(int)position.y,this.width,this.height,null);
+        if (this.image != null) {
+            graphics.drawImage(this.image, (int)position.x - this.image.getWidth() / 2, (int)position.y - this.image.getHeight() / 2, null);
+        }
     }
 }
